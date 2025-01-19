cb811acf93afb992bcb1c78db902a4931246c8ec
Sergiusz Kuderski <skuderski@gmail.com>
2025-01-19 19:06:36 +0100

    initial commit

---
+++ exercise.py
@@ -0,0 +1,10 @@
+def count_letters_in_string(string: str) -> dict:
+    count = {}
+    for letter in string.lower():
+        if letter not in count:
+            count[letter] = 1
+        else:
+            count[letter] +=1
+
+    return count
+print(count_letters_in_string("wodeczkawsad"))
